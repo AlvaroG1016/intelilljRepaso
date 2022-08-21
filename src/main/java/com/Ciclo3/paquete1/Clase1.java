c347247e6d0ff01cbba18bdbe10983fc5757a97f
Alvaro <alvaro.g.b@outlook.com>
2022-08-21 18:38:20 -0500

    Agregué una ñinea de xt

--- src/main/java/com/Ciclo3/paquete1/Clase1.java
+++ src/main/java/com/Ciclo3/paquete1/Clase1.java
@@ -3,5 +3,6 @@ package com.Ciclo3.paquete1;
 public class Clase1 {
     public static void main(String[] args) {
         System.out.println("prueba");
+        System.out.println("dasfads");
     }
 }
