c7a8c78d27ed11ff5e0254ef968087865ec3e460
Alvaro <alvaro.g.b@outlook.com>
2022-08-21 19:11:22 -0500

    nueva rama de facturacion

--- src/main/java/com/Ciclo3/paquete1/Clase1.java
+++ src/main/java/com/Ciclo3/paquete1/Clase1.java
@@ -4,5 +4,6 @@ public class Clase1 {
     public static void main(String[] args) {
         System.out.println("prueba");
         System.out.println("dasfads");
+        System.out.println("facturacion");
     }
 }
